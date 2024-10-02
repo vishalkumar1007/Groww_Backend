75390618a466c29b9aa7f975d643b765bdff9737
vishalkumar1007 <vishalkumarnke93@gmail.com>
2024-10-02 20:30:03 +0530

    get limited stockHead data API created

--- controllers/stockController.js
+++ controllers/stockController.js
@@ -43,7 +43,36 @@ const handelAllStockHead = async (req,res)=>{
     }
 }
 
+const handelToGetStockHeadLimit = async (req,res)=>{
+    try {
+        const {dataLen} = req.query;
+
+        if(!dataLen){
+            console.log('Data len is require in handelToGetStockHeadLimit');
+            res.status(401).json({msg:'expect dataLen in params'});
+            return;
+        }
+
+        const stockHeadDataLimit = await allStockHead.find({}).limit(Number(dataLen));
+
+        if(stockHeadDataLimit){
+            res.status(200).json(stockHeadDataLimit);
+            return;
+        }
+
+        res.status(404).json({msg:'data not found'});
+
+    } catch (error) {
+        console.log('Internal server error on handelAllStockHead')
+        res.status(500).json({ msg: 'Internal server error', status: 'access denied' });
+    }
+}
+
+
+
+
 module.exports = {
     handelToGetStockData,
-    handelAllStockHead
+    handelAllStockHead,
+    handelToGetStockHeadLimit
 }
--- routes/stockRoute.js
+++ routes/stockRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const StockRouter = express.Router();
-const {handelToGetStockData,handelAllStockHead} = require('../controllers/stockController');
+const {handelToGetStockData,handelAllStockHead,handelToGetStockHeadLimit} = require('../controllers/stockController');
 
 StockRouter.get('/getById',handelToGetStockData);
 StockRouter.get('/getAllHead',handelAllStockHead);
+StockRouter.get('/getAllHead/limit',handelToGetStockHeadLimit);
 
 module.exports = StockRouter;
